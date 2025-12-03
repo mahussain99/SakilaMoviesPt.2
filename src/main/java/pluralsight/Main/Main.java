8a27e872569f8ec599c3e487a8ace03221bde707
Family <mh5212357@gmail.com>
2025-12-02 22:54:07 -0500

    Done this exercise

--- src/main/java/pluralsight/Main/Main.java
+++ src/main/java/pluralsight/Main/Main.java
@@ -1,7 +1,67 @@
-package pluralsight;
+package pluralsight.Main;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import pluralsight.db.DataManager;
+import pluralsight.model.Actor;
+import pluralsight.model.Film;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        if (args.length != 2) {
+            System.out.println("Application need to correct username and password");
+        }
+        String username = args[0];
+        String password = args[1];
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        DataManager dataManager = new DataManager(dataSource);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter actor lastName");
+        String lastName = scanner.nextLine();
+
+
+        List<Actor> actorList = dataManager.getAllActorName(lastName);
+        if (!actorList.isEmpty()) {
+            System.out.println("ALl actors name is matching:\n");
+            for (Actor actor : actorList) {
+                System.out.println("Actor Id: " + actor.getActorId());
+                System.out.println("Actor First Name: " + actor.getFirstName());
+                System.out.println("Actor First Name: " + actor.getLastName());
+            }
+
+        } else {
+            System.out.println("Actor name not matching: ");
+        }
+        System.out.println("Enter the actor ID to watch their films");
+        int actorID = scanner.nextInt();
+        scanner.nextLine();
+
+        List<Film> filmList = dataManager.filmByActorId(actorID);
+
+        if (!filmList.isEmpty()) {
+            System.out.println(" films matching for actor id:\n");
+
+            for (Film film : filmList) {
+                System.out.println("Film ID" + film.getFilmId());
+                System.out.println("Title: " + film.getTitle());
+                System.out.println("Description: " + film.getDescription());
+                System.out.println("Release Year: " + film.getReleaseYear());
+                System.out.println("Length: " + film.getLength());
+                System.out.println();
+
+            }
+        } else {
+            System.out.println(" No film matching with actor id");
+        }
 
     }
 }
--- src/main/java/pluralsight/db/DataManager.java
+++ src/main/java/pluralsight/db/DataManager.java
@@ -1,4 +1,81 @@
 package pluralsight.db;
 
+import pluralsight.model.Actor;
+import pluralsight.model.Film;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataManager {
+    private DataSource dataSource;
+
+    public DataManager(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Actor> getAllActorName(String lastName) {
+        List<Actor> actors = new ArrayList<>();
+        String actorQuery = """
+                SELECT actor_id, first_name, last_name
+                FROM actor
+                Where last_name = ?
+                """;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(actorQuery);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            preparedStatement.setString(1, lastName);
+
+            while (resultSet.next()) {
+
+                int actorId = resultSet.getInt(1);
+                String actorFirstName = resultSet.getString(2);
+                String actorLastName = resultSet.getString(3);
+
+                actors.add(new Actor(actorId, actorFirstName, actorLastName));
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Show me run time error");
+            ex.printStackTrace();
+        }
+        return actors;
+    }
+
+    public List<Film> filmByActorId(int actorId) throws SQLException {
+        List<Film> filmList = new ArrayList<>();
+
+        String filmQuery =  """ 
+                             SELECT film.film_id, film.title, film.description, film.release_year, film.length
+                             FROM film
+                             JOIN film_actor ON film.film_id = film_actor.film_id
+                             WHERE film_actor.actor_id = ?
+                       """;
+
+       try ( Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement( filmQuery)){;
+             statement.setInt(1, actorId);
+            try (ResultSet resultSet = statement.executeQuery()){;
+                while (resultSet.next()){
+
+                    int filmId = resultSet.getInt(1);
+                    String title = resultSet.getString(2);
+                    String description = resultSet.getString(3);
+                    int releaseYear = resultSet.getInt(4);
+                    int length = resultSet.getInt(5);
+
+                    filmList.add(new Film(filmId, title, description, releaseYear, length));
+
+
+                }
+            }
+
+        }
+        return filmList;
+    }
 }
